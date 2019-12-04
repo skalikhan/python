1fcb3ba8b21e88d4062f738e104c465cc69253e8
root <root@ip-172-31-29-178.us-east-2.compute.internal>
2019-12-04 06:21:58 +0000

    pythoncode

---
+++ one.py
@@ -0,0 +1,9 @@
+class demo:
+    def __init__(self):
+        self.name="alikhan"
+        self.id=505
+        self.salary=2500000
+    def display(self):
+        print("employee name{} employee id{} employee salary{}".formate(self.name,self.id,self.salary))
+v=demo()
+v.display()
